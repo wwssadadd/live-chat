dd09f88a27c2a9e8239def431472c1306b244879
lucas bai <lucasbai@wentworth.school.nz>
2026-05-09 16:13:23 +1200

    我也不知道我干嘛了 今天5月九号

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 import express from 'express';
 import path from 'path';
-import{ fileURLToPath } from 'url'
+import { fileURLToPath } from 'url'
 const app = express();
-const filename =fileURLToPath(import.meta.url);
+const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 console.log(dirname)
 
-app.use(express.static(path.join(dirname,'public')));
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(dirname, 'public')));
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
+app.set('views', path.join(dirname, 'views'))
 
-app.post('/join',(req,res)=>{
+const data=[];
+app.post('/join', (req, res) => {
       const nickname = req.body.nickname
-      res.send('welcome ' + nickname);
+data.push({
+nickname:'system',
+message:'welcome'+nickname,
+datetime: new date()
+})
+      res.render('chat', { nickname });
 
 })
 
-
-
-app.listen(3000, ()=>{
-    console.log('server running at http://localhost:3000')
+app.listen(3000, () => {
+      console.log('server running at http://localhost:3000')
 })
